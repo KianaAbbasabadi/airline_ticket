4ae4c7cd0228223f393e81bbd56446aafe81b518
niyousha <niyousha48@gmail.com>
2025-07-10 14:58:34 +0330

    تغیر داده شد

--- model/entity/user.py
+++ model/entity/user.py
@@ -1,3 +1,5 @@
+from altair import value
+
 from model.tools.validation import name_validator
 
 
@@ -19,14 +21,47 @@ class User:
     def to_tuple(self):
         return (self.name, self.family, self.birth_date, self.username, self.role, )
 
-    def username (self):
-        return f"{self.username}"
+    def get_name (self):
+        return self.name
 
     def __repr__(self):
         return f"{self.username()} {self.full_name()}"
 
     def to_tuple(self):
         return tuple(self.username().values())
+
+    def set_name(self, value):
+        self._name = value
+
+    def get_family(self):
+        return self.family
+
+    def set_family(self, value):
+        self._family = value
+
+    def get_user_name(self):
+        return self.username
+    def set_user_name(self, value):
+        self._username = value
+
+    def get_password(self):
+        return self._password
+
+    def set_password(self, value):
+        self._password = value
+
+    def get_role(self):
+        return self.role()
+
+    def set_role(self, value):
+        self._role = value
+
+    def is_locked(self):
+        self._is_locked = value
+
+    def set_is_locked(self, value):
+        self._is_locked = value
+
 
 
     @property
